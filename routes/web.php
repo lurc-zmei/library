035f984bc0d8766693719c8dac208db9f0522391
lurc-zmei <lurc-zmei@inbox.ru>
2025-08-20 18:31:16 +0300

    Один ко многим

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $categories = Category::all();
+    $books = Book::all();
+    return view('welcome', compact('categories', 'books'));
 });
